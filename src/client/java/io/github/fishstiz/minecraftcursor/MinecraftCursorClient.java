a370b894550588b75ce0a332a57fecf7a5c23d64
fishstiz <fishstickz7777@gmail.com>
2025-01-11 18:10:42 +0800

    fix deserializable config and moved it to config/minecraft-cursor.json

--- src/client/java/io/github/fishstiz/minecraftcursor/MinecraftCursorClient.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/MinecraftCursorClient.java
@@ -10,7 +10,7 @@ import net.minecraft.resource.ResourceType;
 
 public class MinecraftCursorClient implements ClientModInitializer {
     private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
-    private static final CursorConfigService CONFIG = new CursorConfigService(MinecraftCursor.MOD_ID + "/config" + CursorConfigLoader.FILE_EXTENSION);
+    private static final CursorConfigService CONFIG = new CursorConfigService(String.format("config/%s%s", MinecraftCursor.MOD_ID, CursorConfigLoader.FILE_EXTENSION));
     private static final CursorManager CURSOR_MANAGER = new CursorManager(CONFIG, CLIENT);
     private static final CursorResourceReloadListener RESOURCE_RELOAD_LISTENER = new CursorResourceReloadListener(CURSOR_MANAGER, MinecraftCursor.MOD_ID);
 
