b51336809d3bd4995ae4d7d0c7906a8094b62680
fishstiz <fishstickz7777@gmail.com>
2025-01-24 16:58:16 +0800

    convert HandledScreen to use mixins

--- src/client/java/io/github/fishstiz/minecraftcursor/registry/gui/ingame/HandledScreenCursor.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/registry/gui/ingame/HandledScreenCursor.java
@@ -1,65 +1,31 @@
 package io.github.fishstiz.minecraftcursor.registry.gui.ingame;
 
-import io.github.fishstiz.minecraftcursor.MinecraftCursor;
 import io.github.fishstiz.minecraftcursor.MinecraftCursorClient;
 import io.github.fishstiz.minecraftcursor.config.CursorConfig;
 import io.github.fishstiz.minecraftcursor.cursor.CursorType;
+import io.github.fishstiz.minecraftcursor.mixin.client.HandledScreenAccessor;
 import io.github.fishstiz.minecraftcursor.registry.CursorTypeRegistry;
 import io.github.fishstiz.minecraftcursor.registry.utils.CursorTypeUtils;
-import io.github.fishstiz.minecraftcursor.registry.utils.LookupUtils;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.VarHandle;
-
-public class HandledScreenCursor {
-    protected static final String BACKGROUND_WIDTH_NAME = "field_2792";
-    protected static VarHandle backgroundWidth;
-    protected static final String BACKGROUND_HEIGHT_NAME = "field_2779";
-    protected static VarHandle backgroundHeight;
-    protected static final String HANDLER_NAME = "field_2797";
-    protected static VarHandle screenHandler;
-    protected static final String X_NAME = "field_2776";
-    protected static VarHandle x;
-    protected static final String Y_NAME = "field_2800";
-    protected static VarHandle y;
-    protected static final String FOCUSED_SLOT_NAME = "field_2787";
-    protected static VarHandle focusedSlot;
-    public static final String POINT_W_BOUNDS_NAME = "method_2378";
-    public static final String POINT_W_BOUNDS_DESC = "(IIIIDD)Z";
-    public static MethodHandle isPointWithinBounds;
+public class HandledScreenCursor<T extends ScreenHandler> {
+    private HandledScreenCursor() {
+    }
 
     public static void register(CursorTypeRegistry cursorTypeRegistry) {
-        try {
-            initHandles();
-            cursorTypeRegistry.register(HandledScreen.class, HandledScreenCursor::getCursorType);
-        } catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException e) {
-            MinecraftCursor.LOGGER.warn("Could not register cursor type for HandledScreen");
-        }
+        cursorTypeRegistry.register(HandledScreen.class, new HandledScreenCursor<>()::getCursorType);
     }
 
-    private static void initHandles() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException {
-        Class<?> targetClass = HandledScreen.class;
-        screenHandler = LookupUtils.getVarHandle(targetClass, HANDLER_NAME, ScreenHandler.class);
-        backgroundWidth = LookupUtils.getVarHandle(targetClass, BACKGROUND_WIDTH_NAME, int.class);
-        backgroundHeight = LookupUtils.getVarHandle(targetClass, BACKGROUND_HEIGHT_NAME, int.class);
-        x = LookupUtils.getVarHandle(targetClass, X_NAME, int.class);
-        y = LookupUtils.getVarHandle(targetClass, Y_NAME, int.class);
-        focusedSlot = LookupUtils.getVarHandle(targetClass, FOCUSED_SLOT_NAME, Slot.class);
-        isPointWithinBounds = LookupUtils.getMethodHandle(HandledScreen.class, POINT_W_BOUNDS_NAME, POINT_W_BOUNDS_DESC,
-                boolean.class, int.class, int.class, int.class, int.class, double.class, double.class
-        );
-    }
-
-    public static CursorType getCursorType(Element element, double mouseX, double mouseY) {
+    @SuppressWarnings("unchecked")
+    public CursorType getCursorType(Element element, double mouseX, double mouseY) {
         CursorConfig config = MinecraftCursorClient.CONFIG.get();
 
-        HandledScreen<?> handledScreen = (HandledScreen<?>) element;
-        ScreenHandler handler = (ScreenHandler) screenHandler.get(handledScreen);
-        Slot focusedSlot = (Slot) HandledScreenCursor.focusedSlot.get(handledScreen);
+        HandledScreenAccessor<T> handledScreen = (HandledScreenAccessor<T>) element;
+        ScreenHandler handler = handledScreen.getHandler();
+        Slot focusedSlot = handledScreen.getFocusedSlot();
 
         boolean canClickFocusedSlot = handler.getCursorStack().isEmpty()
                 && focusedSlot != null
