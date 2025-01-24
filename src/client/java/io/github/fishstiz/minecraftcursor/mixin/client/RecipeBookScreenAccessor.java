a15fc4df886bc4f847bac0d141acb7fa6ce1c0bf
fishstiz <fishstickz7777@gmail.com>
2025-01-24 18:00:09 +0800

    convert RecipeBookScreenCursor to use mixin

---
+++ src/client/java/io/github/fishstiz/minecraftcursor/mixin/client/RecipeBookScreenAccessor.java
@@ -0,0 +1,13 @@
+package io.github.fishstiz.minecraftcursor.mixin.client;
+
+import net.minecraft.client.gui.screen.ingame.RecipeBookScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.screen.AbstractRecipeScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(RecipeBookScreen.class)
+public interface RecipeBookScreenAccessor<T extends AbstractRecipeScreenHandler> extends HandledScreenAccessor<T> {
+    @Accessor("recipeBook")
+    RecipeBookWidget<T> getRecipeBook();
+}
---
+++ src/client/java/io/github/fishstiz/minecraftcursor/mixin/client/RecipeBookWidgetAccessor.java
@@ -0,0 +1,33 @@
+package io.github.fishstiz.minecraftcursor.mixin.client;
+
+import net.minecraft.client.gui.screen.recipebook.RecipeBookResults;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.gui.screen.recipebook.RecipeGroupButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.ToggleButtonWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.List;
+
+@Mixin(RecipeBookWidget.class)
+public interface RecipeBookWidgetAccessor {
+    @Invoker("isOpen")
+    boolean isOpen();
+
+    @Accessor("searchField")
+    TextFieldWidget getSearchField();
+
+    @Accessor("toggleCraftableButton")
+    ToggleButtonWidget getToggleCraftableButton();
+
+    @Accessor("tabButtons")
+    List<RecipeGroupButtonWidget> getTabButtons();
+
+    @Accessor("currentTab")
+    RecipeGroupButtonWidget getCurrentTab();
+
+    @Accessor("recipesArea")
+    RecipeBookResults getRecipesArea();
+}
