ad488b6be122302c0ccd0e49f889659f88d8b75b
fishstiz <fishstickz7777@gmail.com>
2025-01-24 18:24:37 +0800

    refactor LoomScreenCursor to use accessors

---
+++ src/client/java/io/github/fishstiz/minecraftcursor/mixin/client/LoomScreenAccessor.java
@@ -0,0 +1,15 @@
+package io.github.fishstiz.minecraftcursor.mixin.client;
+
+import net.minecraft.client.gui.screen.ingame.LoomScreen;
+import net.minecraft.screen.LoomScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LoomScreen.class)
+public interface LoomScreenAccessor extends HandledScreenAccessor<LoomScreenHandler> {
+    @Accessor("canApplyDyePattern")
+    boolean canApplyDyePattern();
+
+    @Accessor("visibleTopRow")
+    int getVisibleTopRow();
+}
