2e0caad715e5398449771aee89461250ddd9fab8
fishstiz <fishstickz7777@gmail.com>
2025-01-24 18:11:05 +0800

    refactor CrafterScreenCursor

--- src/client/java/io/github/fishstiz/minecraftcursor/registry/gui/ingame/CrafterScreenCursor.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/registry/gui/ingame/CrafterScreenCursor.java
@@ -1,7 +1,7 @@
 package io.github.fishstiz.minecraftcursor.registry.gui.ingame;
 
-import io.github.fishstiz.minecraftcursor.MinecraftCursor;
 import io.github.fishstiz.minecraftcursor.cursor.CursorType;
+import io.github.fishstiz.minecraftcursor.mixin.client.HandledScreenAccessor;
 import io.github.fishstiz.minecraftcursor.registry.CursorTypeRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Element;
@@ -11,36 +11,31 @@ import net.minecraft.screen.CrafterScreenHandler;
 import net.minecraft.screen.slot.CrafterInputSlot;
 import net.minecraft.screen.slot.Slot;
 
-public class CrafterScreenCursor extends HandledScreenCursor {
-    public static void register(CursorTypeRegistry cursorTypeRegistry) {
-        if (screenHandler == null || focusedSlot == null) {
-            MinecraftCursor.LOGGER.warn("Cannot register cursor type for CrafterScreen");
-            return;
-        }
-        cursorTypeRegistry.register(CrafterScreen.class, CrafterScreenCursor::getCursorType);
+public class CrafterScreenCursor extends HandledScreenCursor<CrafterScreenHandler> {
+    private CrafterScreenCursor() {
     }
 
-    public static CursorType getCursorType(Element element, double mouseX, double mouseY) {
-        CursorType handledScreenCursor = HandledScreenCursor.getCursorType(element, mouseX, mouseY);
-        if (handledScreenCursor != CursorType.DEFAULT) {
-            return handledScreenCursor;
-        }
+    public static void register(CursorTypeRegistry cursorTypeRegistry) {
+        cursorTypeRegistry.register(CrafterScreen.class, new CrafterScreenCursor()::getCursorType);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public CursorType getCursorType(Element element, double mouseX, double mouseY) {
+        CursorType cursorType = super.getCursorType(element, mouseX, mouseY);
+        if (cursorType != CursorType.DEFAULT) return cursorType;
 
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if (player == null) {
-            return CursorType.DEFAULT;
-        }
+        if (player == null) return CursorType.DEFAULT;
 
-        CrafterScreen crafterScreen = (CrafterScreen) element;
-        CrafterScreenHandler handler = (CrafterScreenHandler) screenHandler.get(crafterScreen);
-        Slot focusedSlot = (Slot) HandledScreenCursor.focusedSlot.get(crafterScreen);
+        HandledScreenAccessor<CrafterScreenHandler> crafterScreen = (HandledScreenAccessor<CrafterScreenHandler>) element;
+        Slot focusedSlot = crafterScreen.getFocusedSlot();
         if (focusedSlot instanceof CrafterInputSlot
-                && handler.getCursorStack().isEmpty()
+                && crafterScreen.getHandler().getCursorStack().isEmpty()
                 && !focusedSlot.hasStack()
                 && !player.isSpectator()) {
             return CursorType.POINTER;
         }
-
         return CursorType.DEFAULT;
     }
 }
