d86c273864501e173cc40bbacbc576ee4592715f
fishstiz <fishstickz7777@gmail.com>
2025-01-25 03:17:08 +0800

    improve element widget detection

--- src/client/java/io/github/fishstiz/minecraftcursor/mixin/client/ScreenMixin.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/mixin/client/ScreenMixin.java
@@ -2,17 +2,23 @@ package io.github.fishstiz.minecraftcursor.mixin.client;
 
 import io.github.fishstiz.minecraftcursor.MinecraftCursorClient;
 import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClickableWidget.class)
-public abstract class ElementMixin {
+import java.util.List;
+
+@Mixin(Screen.class)
+public abstract class ScreenMixin {
+    @Shadow @Final private List<Element> children;
+
     @Inject(method = "<init>", at = @At("TAIL"))
-    public void init(int i, int j, int k, int l, Text text, CallbackInfo ci) {
-        MinecraftCursorClient.SCREEN_ELEMENTS.add((Element) this);
+    public void init(Text title, CallbackInfo ci) {
+        MinecraftCursorClient.SCREEN_ELEMENTS.addAll(children);
     }
 }
