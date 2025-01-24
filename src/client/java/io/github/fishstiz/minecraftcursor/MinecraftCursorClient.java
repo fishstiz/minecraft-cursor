0c2f109d86203ea517d9b8a3ea539790291644e5
fishstiz <fishstickz7777@gmail.com>
2025-01-25 03:17:08 +0800

    improve element widget detection

--- src/client/java/io/github/fishstiz/minecraftcursor/MinecraftCursorClient.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/MinecraftCursorClient.java
@@ -5,7 +5,9 @@ import io.github.fishstiz.minecraftcursor.config.CursorConfigService;
 import io.github.fishstiz.minecraftcursor.cursor.CursorManager;
 import io.github.fishstiz.minecraftcursor.cursor.CursorType;
 import io.github.fishstiz.minecraftcursor.registry.CursorTypeRegistry;
+import io.github.fishstiz.minecraftcursor.utils.CursorTypeUtils;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.client.MinecraftClient;
@@ -14,8 +16,7 @@ import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.resource.ResourceType;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 public class MinecraftCursorClient implements ClientModInitializer {
     public static final MinecraftClient CLIENT = MinecraftClient.getInstance();
@@ -23,7 +24,9 @@ public class MinecraftCursorClient implements ClientModInitializer {
             new CursorConfigService(String.format("config/%s%s", MinecraftCursor.MOD_ID, CursorConfigLoader.FILE_EXTENSION));
     public static final CursorManager CURSOR_MANAGER = new CursorManager(CONFIG, CLIENT);
     public static final CursorTypeRegistry CURSOR_REGISTRY = new CursorTypeRegistry();
-    public static final List<Element> SCREEN_ELEMENTS = new ArrayList<>();
+    public static final HashSet<Element> SCREEN_ELEMENTS = new HashSet<>();
+
+    private Screen visibleNonCurrentScreen;
 
     @Override
     public void onInitializeClient() {
@@ -32,36 +35,52 @@ public class MinecraftCursorClient implements ClientModInitializer {
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(resourceReloadListener);
 
         ScreenEvents.BEFORE_INIT.register((MinecraftClient client, Screen screen, int width, int height) -> {
-            if (client.currentScreen == null) return;
             SCREEN_ELEMENTS.clear();
-            ScreenEvents.afterRender(client.currentScreen).register(this::afterRender);
+
+            if (client.currentScreen == null) {
+                CURSOR_MANAGER.setCurrentCursor(CursorType.DEFAULT);
+                visibleNonCurrentScreen = screen;
+                return;
+            }
+
+            visibleNonCurrentScreen = null;
+            ScreenEvents.afterRender(client.currentScreen).register(this::afterRenderScreen);
         });
+
+        ClientTickEvents.START_CLIENT_TICK.register(this::tick);
     }
 
-    public void afterRender(Screen currentScreen, DrawContext context, int mouseX, int mouseY, float tickDelta) {
-        if (!CURSOR_MANAGER.isAdaptive()) {
-            CURSOR_MANAGER.setCurrentCursor(CursorType.DEFAULT);
-            return;
-        }
-        if (currentScreen.isDragging() && CURSOR_MANAGER.getCurrentCursor().getType() == CursorType.GRABBING) {
-            return;
-        }
+    public void afterRenderScreen(Screen currentScreen, DrawContext context, int mouseX, int mouseY, float tickDelta) {
+        CURSOR_MANAGER.setCurrentCursor(getCursorType(currentScreen, mouseX, mouseY));
+    }
 
-        CursorType cursorType = CursorType.DEFAULT;
-        for (Element element : SCREEN_ELEMENTS) {
-            if (element.isMouseOver(mouseX, mouseY)) {
-                cursorType = CURSOR_REGISTRY.getCursorType(element, mouseX, mouseY);
-            }
+    public void tick(MinecraftClient client) {
+        if (client.currentScreen == null && visibleNonCurrentScreen != null && !SCREEN_ELEMENTS.isEmpty()) {
+            double scale = client.getWindow().getScaleFactor();
+            double mouseX = client.mouse.getX() / scale;
+            double mouseY = client.mouse.getY() / scale;
+            CURSOR_MANAGER.setCurrentCursor(getCursorType(visibleNonCurrentScreen, mouseX, mouseY));
         }
-        if (cursorType == CursorType.DEFAULT) {
-            cursorType = CURSOR_REGISTRY.getCursorType(currentScreen, mouseX, mouseY);
-        }
-        if (cursorType == CursorType.DEFAULT) {
-            cursorType = currentScreen.hoveredElement(mouseX, mouseY)
-                    .map(element -> CURSOR_REGISTRY.getCursorType(element, mouseX, mouseY))
-                    .orElse(CursorType.DEFAULT);
-        }
+    }
 
-        CURSOR_MANAGER.setCurrentCursor(cursorType);
+    private CursorType getCursorType(Screen currentScreen, double mouseX, double mouseY) {
+        if (!CURSOR_MANAGER.isAdaptive()) return CursorType.DEFAULT;
+
+        if (CursorTypeUtils.isGrabbing()) return CursorType.GRABBING;
+
+        CursorType cursorType = CURSOR_REGISTRY.getCursorType(currentScreen, mouseX, mouseY);
+        cursorType = cursorType != CursorType.DEFAULT ? cursorType
+                : SCREEN_ELEMENTS.stream()
+                .filter(element -> element.isMouseOver(mouseX, mouseY))
+                .map(element -> CURSOR_REGISTRY.getCursorType(element, mouseX, mouseY))
+                .filter(type -> type != CursorType.DEFAULT)
+                .findFirst()
+                .orElse(CursorType.DEFAULT);
+        cursorType = cursorType != CursorType.DEFAULT ? cursorType
+                : currentScreen.hoveredElement(mouseX, mouseY)
+                .map(element -> CURSOR_REGISTRY.getCursorType(element, mouseX, mouseY))
+                .orElse(CursorType.DEFAULT);
+
+        return cursorType;
     }
 }
