e3ca89faaca8fba5488c8a3acc2673e7515c9b38
fishstiz <fishstickz7777@gmail.com>
2025-01-24 18:19:23 +0800

    refactor EnchantmentScreenCursor

--- src/client/java/io/github/fishstiz/minecraftcursor/registry/gui/ingame/EnchantmentScreenCursor.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/registry/gui/ingame/EnchantmentScreenCursor.java
@@ -1,8 +1,8 @@
 package io.github.fishstiz.minecraftcursor.registry.gui.ingame;
 
-import io.github.fishstiz.minecraftcursor.MinecraftCursor;
 import io.github.fishstiz.minecraftcursor.MinecraftCursorClient;
 import io.github.fishstiz.minecraftcursor.cursor.CursorType;
+import io.github.fishstiz.minecraftcursor.mixin.client.HandledScreenAccessor;
 import io.github.fishstiz.minecraftcursor.registry.CursorTypeRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Element;
@@ -10,37 +10,35 @@ import net.minecraft.client.gui.screen.ingame.EnchantmentScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.screen.EnchantmentScreenHandler;
 
-public class EnchantmentScreenCursor extends HandledScreenCursor {
-    // Derived from EnchantmentScreen.drawBackground()
+public class EnchantmentScreenCursor extends HandledScreenCursor<EnchantmentScreenHandler> {
+    // Derived from EnchantmentScreen#drawBackground
     public static final int ENCHANTMENT_BTN_WIDTH = 108;
     public static final int ENCHANTMENT_BTN_HEIGHT = 19;
     public static final int ENCHANTMENT_BTN_OFFSET_X = 60;
     public static final int ENCHANTMENT_BTN_OFFSET_Y = 14;
 
-    public static void register(CursorTypeRegistry cursorTypeRegistry) {
-        if (screenHandler == null || backgroundWidth == null || backgroundHeight == null) {
-            MinecraftCursor.LOGGER.warn("Could not register cursor type for EnchantmentScreen");
-            return;
-        }
-        cursorTypeRegistry.register(EnchantmentScreen.class, EnchantmentScreenCursor::getCursorType);
+    private EnchantmentScreenCursor() {
     }
 
-    public static CursorType getCursorType(Element element, double mouseX, double mouseY) {
-        CursorType handledScreenCursor = HandledScreenCursor.getCursorType(element, mouseX, mouseY);
-        if (handledScreenCursor != CursorType.DEFAULT) {
-            return handledScreenCursor;
-        }
+    public static void register(CursorTypeRegistry cursorTypeRegistry) {
+        cursorTypeRegistry.register(EnchantmentScreen.class, new EnchantmentScreenCursor()::getCursorType);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public CursorType getCursorType(Element element, double mouseX, double mouseY) {
+        CursorType cursorType = super.getCursorType(element, mouseX, mouseY);
+        if (cursorType != CursorType.DEFAULT) return cursorType;
 
         if (!MinecraftCursorClient.CONFIG.get().isEnchantmentsEnabled()) return CursorType.DEFAULT;
 
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if (player == null) {
-            return CursorType.DEFAULT;
-        }
+        if (player == null) return CursorType.DEFAULT;
 
         EnchantmentScreen enchantmentScreen = (EnchantmentScreen) element;
-        EnchantmentScreenHandler handler =
-                (EnchantmentScreenHandler) screenHandler.get(enchantmentScreen);
+        HandledScreenAccessor<EnchantmentScreenHandler> accessor =
+                (HandledScreenAccessor<EnchantmentScreenHandler>) enchantmentScreen;
+        EnchantmentScreenHandler handler = accessor.getHandler();
 
         for (int i = 0; i < handler.enchantmentPower.length; i++) {
             int enchantmentPower = handler.enchantmentPower[i];
@@ -52,17 +50,17 @@ public class EnchantmentScreenCursor extends HandledScreenCursor {
                 continue;
             }
 
-            int containerX = (enchantmentScreen.width - (int) backgroundWidth.get(enchantmentScreen)) / 2;
-            int containerY = (enchantmentScreen.height - (int) backgroundHeight.get(enchantmentScreen)) / 2;
+            int containerX = (enchantmentScreen.width - accessor.getBackgroundWidth()) / 2;
+            int containerY = (enchantmentScreen.height - accessor.getBackgroundHeight()) / 2;
             int buttonX = containerX + ENCHANTMENT_BTN_OFFSET_X;
             int buttonY = containerY + ENCHANTMENT_BTN_OFFSET_Y + ENCHANTMENT_BTN_HEIGHT * i;
             int relativeX = (int) mouseX - buttonX;
             int relativeY = (int) mouseY - buttonY;
 
-            boolean isPointWithinBounds = relativeX >= 0 && relativeY >= 0 &&
-                    relativeX < ENCHANTMENT_BTN_WIDTH && relativeY < ENCHANTMENT_BTN_HEIGHT;
-
-            if (isPointWithinBounds) {
+            if (relativeX >= 0
+                    && relativeY >= 0
+                    && relativeX < ENCHANTMENT_BTN_WIDTH
+                    && relativeY < ENCHANTMENT_BTN_HEIGHT) {
                 return CursorType.POINTER;
             }
         }
