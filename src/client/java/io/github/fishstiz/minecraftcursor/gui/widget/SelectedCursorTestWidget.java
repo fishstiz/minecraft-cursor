1ff6ad24b49077e55290d4c673ca88648c6163b8
fishstiz <fishstickz7777@gmail.com>
2025-02-03 01:28:04 +0800

    implement CursorProvider on created widgets as example

--- src/client/java/io/github/fishstiz/minecraftcursor/gui/widget/SelectedCursorTestWidget.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/gui/widget/SelectedCursorTestWidget.java
@@ -1,8 +1,8 @@
 package io.github.fishstiz.minecraftcursor.gui.widget;
 
 import io.github.fishstiz.minecraftcursor.MinecraftCursor;
+import io.github.fishstiz.minecraftcursor.api.CursorProvider;
 import io.github.fishstiz.minecraftcursor.cursor.Cursor;
-import io.github.fishstiz.minecraftcursor.cursor.CursorManager;
 import io.github.fishstiz.minecraftcursor.cursor.CursorType;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
@@ -10,12 +10,12 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-public class SelectedCursorTestWidget extends SelectedCursorClickableWidget {
+public class SelectedCursorTestWidget extends SelectedCursorClickableWidget implements CursorProvider {
     private static final Identifier BACKGROUND = Identifier.of(MinecraftCursor.MOD_ID, "textures/cursors/test_background.png");
     private static final int HOTSPOT_RULER_COLOR = 0xFF00FF00; // green
-    private static final ButtonWidget hoverButton = ButtonWidget.builder(Text.empty(),
-            b -> b.setFocused(false)).size(20, 20).build();
     private final SelectedCursorOptionsWidget optionsWidget;
+    ButtonWidget hoverButton = ButtonWidget.builder(Text.empty(),
+            b -> b.setFocused(false)).size(20, 20).build();
 
     public SelectedCursorTestWidget(int size, SelectedCursorOptionsWidget optionsWidget) {
         super(optionsWidget.getX(), optionsWidget.getY(), size, size, Text.empty());
@@ -38,7 +38,7 @@ public class SelectedCursorTestWidget extends SelectedCursorClickableWidget {
 
         if (cursor.getEnabled()) {
             hoverButton.render(context, mouseX, mouseY, delta);
-            renderRuler(context, mouseX, mouseY, cursor.getType());
+            renderRuler(context, mouseX, mouseY);
         } else {
             context.fill(getX(), getY(), getRight(), getBottom(), 0x7F000000); // 50% black overlay
         }
@@ -46,16 +46,16 @@ public class SelectedCursorTestWidget extends SelectedCursorClickableWidget {
         context.drawBorder(getX(), getY(), getWidth(), getHeight(), 0xFF000000);
     }
 
-    private void renderRuler(DrawContext context, int mouseX, int mouseY, CursorType type) {
-        CursorManager cursorManager = optionsWidget.optionsScreen.getCursorManager();
-
+    private void renderRuler(DrawContext context, int mouseX, int mouseY) {
         if (isMouseOver(mouseX, mouseY)) {
             context.drawHorizontalLine(getX(), getRight() - 1, mouseY, HOTSPOT_RULER_COLOR);
             context.drawVerticalLine(mouseX, getY(), getBottom(), HOTSPOT_RULER_COLOR);
-            cursorManager.overrideCurrentCursor(type, -2);
-        } else {
-            cursorManager.removeOverride(-2);
         }
+    }
+
+    @Override
+    public CursorType getCursorType(double mouseX, double mouseY) {
+        return optionsWidget.optionsScreen.getSelectedCursor().getType();
     }
 
     @Override
