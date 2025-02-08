bbc88f3ddbd04d246b0befca13aa0cf011cff1c8
fishstiz <fishstickz7777@gmail.com>
2025-02-09 01:15:36 +0800

    refactor cursors

--- src/client/java/io/github/fishstiz/minecraftcursor/CursorTypeRegistry.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/CursorTypeRegistry.java
@@ -13,17 +13,19 @@ public class CursorTypeRegistry implements CursorTypeRegistrar {
     private static final LinkedHashMap<String, CursorType> TYPES = new LinkedHashMap<>();
 
     CursorTypeRegistry() {
+        register(StandardCursorType.values());
     }
 
-    static {
-        for (CursorType cursorType : StandardCursorType.values()) {
+    @Override
+    public void register(CursorType... cursorTypes) {
+        for (CursorType cursorType : cursorTypes) {
             TYPES.put(cursorType.getKey(), cursorType);
         }
     }
 
     @Override
-    public void register(CursorType cursorType) {
-        TYPES.put(cursorType.getKey(), cursorType);
+    public CursorType register(String key) {
+        return TYPES.computeIfAbsent(key, CursorType::of);
     }
 
     public static Collection<CursorType> types() {
--- src/client/java/io/github/fishstiz/minecraftcursor/gui/screen/CursorOptionsScreen.java
+++ src/client/java/io/github/fishstiz/minecraftcursor/gui/screen/CursorOptionsScreen.java
@@ -122,7 +122,7 @@ public class CursorOptionsScreen extends Screen {
                 selectedCursor.getScale(),
                 selectedCursor.getXhot(),
                 selectedCursor.getYhot(),
-                selectedCursor.getEnabled()
+                selectedCursor.isEnabled()
         );
     }
 
