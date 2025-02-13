4d4e4295126cff6d2cb6758d9a236b021f00954e
fishstiz <fishstickz7777@gmail.com>
2025-02-13 18:40:43 +0800

    expose HandledScreenCursorHandler in API

--- src/main/java/io/github/fishstiz/minecraftcursor/cursorhandler/ingame/HandledScreenCursorHandler.java
+++ src/main/java/io/github/fishstiz/minecraftcursor/cursorhandler/ingame/HandledScreenCursorHandler.java
@@ -1,7 +1,8 @@
 package io.github.fishstiz.minecraftcursor.cursorhandler.ingame;
 
-import io.github.fishstiz.minecraftcursor.api.CursorHandler;
 import io.github.fishstiz.minecraftcursor.api.CursorType;
+import io.github.fishstiz.minecraftcursor.api.cursorhandler.AbstractHandledScreenCursorHandler;
+import io.github.fishstiz.minecraftcursor.impl.HandleScreenCursorProvider;
 import io.github.fishstiz.minecraftcursor.mixin.client.access.HandledScreenAccessor;
 import io.github.fishstiz.minecraftcursor.util.CursorTypeUtil;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -10,9 +11,15 @@ import net.minecraft.screen.slot.Slot;
 
 import static io.github.fishstiz.minecraftcursor.MinecraftCursor.CONFIG;
 
-public class HandledScreenCursorHandler<T extends ScreenHandler, U extends HandledScreen<? extends T>> implements CursorHandler<U> {
+public class HandledScreenCursorHandler<T extends ScreenHandler, U extends HandledScreen<? extends T>> extends AbstractHandledScreenCursorHandler<T, U> {
     @Override
     public CursorType getCursorType(U handledScreen, double mouseX, double mouseY) {
+        CursorType cursorType = computeCursorType(handledScreen);
+        HandleScreenCursorProvider.setCursorType(cursorType);
+        return cursorType;
+    }
+
+    private CursorType computeCursorType(U handledScreen) {
         ScreenHandler handler = ((HandledScreenAccessor<?>) handledScreen).getHandler();
         Slot focusedSlot = ((HandledScreenAccessor<?>) handledScreen).getFocusedSlot();
 
