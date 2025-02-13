4b825953a49af50ec68697d8db29b82e78fba0bc
fishstiz <fishstickz7777@gmail.com>
2025-02-13 17:29:18 +0800

    merge split sources

--- src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursor.java
+++ src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursor.java
@@ -1,15 +1,115 @@
 package io.github.fishstiz.minecraftcursor;
 
-import net.fabricmc.api.ModInitializer;
-
+import io.github.fishstiz.minecraftcursor.api.CursorType;
+import io.github.fishstiz.minecraftcursor.api.MinecraftCursorInitializer;
+import io.github.fishstiz.minecraftcursor.config.CursorConfig;
+import io.github.fishstiz.minecraftcursor.config.CursorConfigLoader;
+import io.github.fishstiz.minecraftcursor.cursor.CursorControllerImpl;
+import io.github.fishstiz.minecraftcursor.impl.CursorControllerProvider;
+import io.github.fishstiz.minecraftcursor.util.CursorTypeUtil;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.resource.ResourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MinecraftCursor implements ModInitializer {
-	public static final String MOD_ID = "minecraft-cursor";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+import java.io.File;
 
-	@Override
-	public void onInitialize() {
-	}
+public class MinecraftCursor implements ClientModInitializer {
+    public static final String MOD_ID = "minecraft-cursor";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private static final CursorTypeRegistry CURSOR_REGISTRY = new CursorTypeRegistry();
+    private static final CursorTypeResolver CURSOR_RESOLVER = new CursorTypeResolver();
+    public static final CursorConfig CONFIG = CursorConfigLoader
+            .fromFile(new File(FabricLoader.getInstance().getConfigDir().toString(), MOD_ID + ".json"));
+    private static CursorManager cursorManager;
+    private static CursorType singleCycleCursor;
+    private Screen visibleNonCurrentScreen;
+
+    @Override
+    public void onInitializeClient() {
+        FabricLoader.getInstance().getEntrypointContainers(MOD_ID, MinecraftCursorInitializer.class).forEach(entrypoint -> {
+            try {
+                entrypoint.getEntrypoint().init(CURSOR_REGISTRY, CURSOR_RESOLVER);
+            } catch (Throwable e) {
+                LOGGER.error(
+                        "Invalid implementation of MinecraftCursorInitializer in mod: {}",
+                        entrypoint.getProvider().getMetadata().getId()
+                );
+            }
+        });
+
+        initCursorManager();
+        CursorResourceReloadListener reloadListener = new CursorResourceReloadListener(cursorManager, MOD_ID, CONFIG);
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(reloadListener);
+        CursorControllerProvider.init(CursorControllerImpl.getInstance());
+
+        ScreenEvents.BEFORE_INIT.register(this::beforeScreenInit);
+        ClientTickEvents.START_CLIENT_TICK.register(this::tick);
+    }
+
+    private static void initCursorManager() {
+        cursorManager = new CursorManager(MinecraftClient.getInstance());
+    }
+
+    private void beforeScreenInit(MinecraftClient client, Screen screen, int width, int height) {
+        if (client.currentScreen == null) {
+            cursorManager.setCurrentCursor(CursorType.DEFAULT);
+            visibleNonCurrentScreen = screen;
+            return;
+        }
+        visibleNonCurrentScreen = null;
+        ScreenEvents.afterRender(client.currentScreen).register(this::afterRenderScreen);
+    }
+
+    private void afterRenderScreen(Screen currentScreen, DrawContext context, int mouseX, int mouseY, float tickDelta) {
+        cursorManager.setCurrentCursor(getCursorType(currentScreen, mouseX, mouseY));
+    }
+
+    private void tick(MinecraftClient client) {
+        if (client.currentScreen == null && visibleNonCurrentScreen != null && !client.mouse.isCursorLocked()) {
+            double scale = client.getWindow().getScaleFactor();
+            double mouseX = client.mouse.getX() / scale;
+            double mouseY = client.mouse.getY() / scale;
+            cursorManager.setCurrentCursor(getCursorType(visibleNonCurrentScreen, mouseX, mouseY));
+        }
+    }
+
+    private CursorType getCursorType(Screen currentScreen, double mouseX, double mouseY) {
+        if (!cursorManager.isAdaptive()) return CursorType.DEFAULT;
+
+        if (CursorTypeUtil.isGrabbing()) return CursorType.GRABBING;
+
+        if (singleCycleCursor != null) {
+            CursorType temp = singleCycleCursor;
+            setSingleCycleCursor(null);
+            return temp;
+        }
+
+        CursorType cursorType = CURSOR_RESOLVER.getCursorType(currentScreen, mouseX, mouseY);
+        cursorType = cursorType != CursorType.DEFAULT ? cursorType
+                : currentScreen.hoveredElement(mouseX, mouseY)
+                .map(element -> CURSOR_RESOLVER.getCursorType(element, mouseX, mouseY))
+                .orElse(CursorType.DEFAULT);
+
+        return cursorType;
+    }
+
+    public static void setSingleCycleCursor(CursorType cursorType) {
+        singleCycleCursor = cursorType;
+    }
+
+    public static CursorManager getCursorManager() {
+        if (cursorManager == null) {
+            throw new IllegalStateException("Cursor Manager not yet initialized");
+        }
+        return cursorManager;
+    }
 }
---
+++ src/main/java/io/github/fishstiz/minecraftcursor/gui/widget/SelectedCursorHotspotWidget.java
@@ -0,0 +1,85 @@
+package io.github.fishstiz.minecraftcursor.gui.widget;
+
+import io.github.fishstiz.minecraftcursor.MinecraftCursor;
+import io.github.fishstiz.minecraftcursor.api.CursorProvider;
+import io.github.fishstiz.minecraftcursor.api.CursorType;
+import io.github.fishstiz.minecraftcursor.util.CursorTypeUtil;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class SelectedCursorHotspotWidget extends ClickableWidget implements CursorProvider {
+    private static final Identifier BACKGROUND = Identifier.of(MinecraftCursor.MOD_ID, "textures/gui/hotspot_background.png");
+    private static final int HOTSPOT_RULER_COLOR = 0xFFFF0000; // red
+    private static final int CURSOR_SIZE = 32;
+    private final SelectedCursorOptionsWidget optionsWidget;
+
+    public SelectedCursorHotspotWidget(int size, SelectedCursorOptionsWidget optionsWidget) {
+        super(optionsWidget.getX(), optionsWidget.getY(), size, size, Text.empty());
+        this.optionsWidget = optionsWidget;
+    }
+
+    @Override
+    protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
+        drawTexture(context, BACKGROUND);
+        drawTexture(context, optionsWidget.optionsScreen.getSelectedCursor().getSprite());
+        renderRuler(context);
+        context.drawBorder(getX(), getY(), getWidth(), getHeight(), 0xFF000000);
+    }
+
+    private void drawTexture(DrawContext context, Identifier sprite) {
+        context.drawTexture(sprite, getX(), getY(), 0, 0, width, height, width, height);
+    }
+
+    private void renderRuler(DrawContext context) {
+        int xhot = (int) optionsWidget.xhotSlider.getTranslatedValue();
+        int yhot = (int) optionsWidget.yhotSlider.getTranslatedValue();
+
+        int rulerSize = getRulerSize();
+        int xhotX1 = (getX() + xhot * rulerSize);
+        int xhotX2 = (getX() + xhot * rulerSize) + rulerSize;
+        int yhotY1 = (getY() + yhot * rulerSize);
+        int yhotY2 = (getY() + yhot * rulerSize) + rulerSize;
+
+        context.fill(xhotX1, getY(), xhotX2, getBottom(), HOTSPOT_RULER_COLOR);
+        context.fill(getX(), yhotY1, getRight(), yhotY2, HOTSPOT_RULER_COLOR);
+    }
+
+    @Override
+    public void onClick(double mouseX, double mouseY) {
+        setHotspots(mouseX, mouseY);
+    }
+
+    @Override
+    protected void onDrag(double mouseX, double mouseY, double deltaX, double deltaY) {
+        setHotspots(mouseX, mouseY);
+    }
+
+    public void setHotspots(double mouseX, double mouseY) {
+        int rulerSize = getRulerSize();
+
+        int xhot = ((int) mouseX - getX()) / rulerSize;
+        int yhot = ((int) mouseY - getY()) / rulerSize;
+
+        optionsWidget.xhotSlider.setValue(xhot);
+        optionsWidget.yhotSlider.setValue(yhot);
+    }
+
+    private int getRulerSize() {
+        return getWidth() / CURSOR_SIZE;
+    }
+
+    @Override
+    public CursorType getCursorType(double mouseX, double mouseY) {
+        if (CursorTypeUtil.isLeftClickHeld() || CursorTypeUtil.isGrabbing()) {
+            return CursorType.GRABBING;
+        }
+        return CursorType.POINTER;
+    }
+
+    @Override
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
+    }
+}
