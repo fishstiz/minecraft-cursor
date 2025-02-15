c825b4073020985fb08091663a2d01eb83f28b5b
fishstiz <fishstickz7777@gmail.com>
2025-02-16 03:15:09 +0800

    added animation modes

--- src/main/java/io/github/fishstiz/minecraftcursor/config/AnimatedCursorConfig.java
+++ src/main/java/io/github/fishstiz/minecraftcursor/config/AnimatedCursorConfig.java
@@ -1,24 +1,28 @@
 package io.github.fishstiz.minecraftcursor.config;
 
+import io.github.fishstiz.minecraftcursor.cursor.AnimationMode;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class AnimatedCursorConfig {
     private static final int MIN_TIME = 1;
+    public final AnimationMode mode;
     public final int frametime;
     public final List<Frame> frames = new ArrayList<>();
 
     public AnimatedCursorConfig() {
+        this.mode = AnimationMode.LOOP;
         this.frametime = MIN_TIME;
     }
 
     public int getTime(int index) {
         for (Frame frame : frames) {
             if (frame.index == index) {
-                return frame.time;
+                return frame.time > 0 ? frame.time : Math.max(frametime, MIN_TIME);
             }
         }
-        return frametime;
+        return Math.max(frametime, MIN_TIME);
     }
 
     public static class Frame {
