a15050c9a37b78c08f5336c02016c09d08a9e5ea
fishstiz <fishstickz7777@gmail.com>
2025-03-15 11:53:29 +0800

    remove GLFWImage Mixin

--- common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/Cursor.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/Cursor.java
@@ -2,7 +2,7 @@ package io.github.fishstiz.minecraftcursor.cursor;
 
 import io.github.fishstiz.minecraftcursor.MinecraftCursor;
 import io.github.fishstiz.minecraftcursor.api.CursorType;
-import io.github.fishstiz.minecraftcursor.compat.glfw.GlfwImageScope;
+import io.github.fishstiz.minecraftcursor.compat.ExternalCursorTracker;
 import io.github.fishstiz.minecraftcursor.config.CursorConfig;
 import io.github.fishstiz.minecraftcursor.util.BufferedImageUtil;
 import io.github.fishstiz.minecraftcursor.util.SettingsUtil;
@@ -72,13 +72,13 @@ public class Cursor {
         int scaledYHot = scale == 1 ? yhot : (int) Math.round(yhot * scale);
 
         GLFWImage glfwImage = GLFWImage.create();
-        ((GlfwImageScope) glfwImage).minecraft_cursor$setInternal(true);
         glfwImage.width(scaledImage.getWidth());
         glfwImage.height(scaledImage.getHeight());
         glfwImage.pixels(BufferedImageUtil.getPixelsRGBA(scaledImage));
         scaledImage.flush();
 
         long previousId = this.id;
+        ExternalCursorTracker.get().storeAddress(glfwImage.address());
         this.id = GLFW.glfwCreateCursor(glfwImage, scaledXHot, scaledYHot);
 
         if (onLoad != null) {
