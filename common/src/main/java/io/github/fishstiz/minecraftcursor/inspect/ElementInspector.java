9167ef0ad848a482dc6b41b439c4230cf0e8435b
fishstiz <fishstickz7777@gmail.com>
2025-05-18 11:48:38 +0800

    nuts

--- common/src/main/java/io/github/fishstiz/minecraftcursor/inspect/ElementInspector.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/inspect/ElementInspector.java
@@ -4,29 +4,18 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
+import org.jetbrains.annotations.NotNull;
 
 public interface ElementInspector {
     default void destroy() {
         // no-op
     }
 
-    default boolean isEnabled() {
+    default boolean setFocused(GuiEventListener hovered, boolean cached) {
         return false;
     }
 
-    default boolean setHovered(GuiEventListener hovered, boolean cached) {
-        return false;
-    }
-
-    default void renderDeepest(Minecraft minecraft, GuiGraphics guiGraphics, Screen screen, double mouseX, double mouseY) {
-        // no-op
-    }
-
-    default void renderHovered(Minecraft minecraft, GuiGraphics guiGraphics) {
-        // no-op
-    }
-
-    default void renderCacheSize(Minecraft minecraft, GuiGraphics guiGraphics) {
+    default void render(Minecraft minecraft, @NotNull Screen screen, GuiGraphics guiGraphics, double mouseX, double mouseY) {
         // no-op
     }
 
