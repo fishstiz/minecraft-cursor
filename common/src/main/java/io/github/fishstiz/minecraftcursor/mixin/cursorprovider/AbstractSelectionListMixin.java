e9f54c34f0fb507b8e5f0aa83942b3a05256ec17
fishstiz <fishstickz7777@gmail.com>
2025-05-19 19:58:19 +0800

    force default cursor when scrolling

---
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/mixin/cursorprovider/AbstractSelectionListMixin.java
@@ -0,0 +1,28 @@
+package io.github.fishstiz.minecraftcursor.mixin.cursorprovider;
+
+import io.github.fishstiz.minecraftcursor.api.CursorController;
+import io.github.fishstiz.minecraftcursor.api.CursorType;
+import io.github.fishstiz.minecraftcursor.util.CursorTypeUtil;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractSelectionList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractSelectionList.class)
+public abstract class AbstractSelectionListMixin {
+    @Shadow
+    private boolean scrolling;
+
+    @Shadow
+    public abstract int getMaxScroll();
+
+    @Inject(method = "render", at = @At(value = "TAIL"))
+    public void forceDefaultCursor(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        if (this.scrolling && this.getMaxScroll() > 0 && CursorTypeUtil.isLeftClickHeld()) {
+            CursorController.getInstance().setSingleCycleCursor(CursorType.DEFAULT_FORCE);
+        }
+    }
+}
