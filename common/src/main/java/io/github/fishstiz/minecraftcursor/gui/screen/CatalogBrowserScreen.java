c61adbc299c0472d89b4b6bbfe74272a0e5aede4
fishstiz <fishstickz7777@gmail.com>
2025-06-03 22:00:44 +0800

    add clear cache, issues link, and wiki under debug

--- common/src/main/java/io/github/fishstiz/minecraftcursor/gui/screen/CatalogBrowserScreen.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/gui/screen/CatalogBrowserScreen.java
@@ -767,6 +767,10 @@ public abstract class CatalogBrowserScreen extends Screen {
             this.catalog.selectItem(item);
         }
 
+        protected CatalogBrowserScreen getScreen() {
+            return this.catalog;
+        }
+
         protected Minecraft getMinecraft() {
             return this.minecraft;
         }
