d5910c1a8fedb20b435f14d5b834dd479f92a1df
fishstiz <fishstickz7777@gmail.com>
2025-06-13 00:01:50 +0800

    minor animation abstraction

--- common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/AnimatedCursor.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/AnimatedCursor.java
@@ -130,6 +130,10 @@ public class AnimatedCursor extends Cursor {
         }
     }
 
+    public Frame nextFrame(AnimationState state) {
+        return this.getFrame(state.next(this));
+    }
+
     public boolean isAnimated() {
         return this.animated;
     }
