87551dceb726a0eb1d86f54a39107ae2a39be2a6
fishstiz <fishstickz7777@gmail.com>
2025-06-13 00:09:06 +0800

    conditionally reload cursor on resize

--- common/src/main/java/io/github/fishstiz/minecraftcursor/mixin/MinecraftMixin.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/mixin/MinecraftMixin.java
@@ -1,6 +1,8 @@
 package io.github.fishstiz.minecraftcursor.mixin;
 
+import io.github.fishstiz.minecraftcursor.MinecraftCursor;
 import io.github.fishstiz.minecraftcursor.cursor.CursorManager;
+import io.github.fishstiz.minecraftcursor.util.SettingsUtil;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MinecraftMixin {
     @Inject(method = "resizeDisplay", at = @At("TAIL"))
     public void reloadCursorsOnResize(CallbackInfo ci) {
-        CursorManager.INSTANCE.reloadCursors();
+        CursorManager.INSTANCE.getCursors().forEach(cursor -> {
+            if (cursor.isEnabled() && SettingsUtil.isAutoScale(cursor.getScale())) {
+                MinecraftCursor.LOGGER.info("[minecraft-cursor] Reloading auto-scaled cursor on resize: '{}'", cursor.getTypeKey());
+                cursor.reload();
+            }
+        });
     }
 }
