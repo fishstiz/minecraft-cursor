f0566ccdea58e2ec2a3746095f215df6f00fff21
fishstiz <fishstickz7777@gmail.com>
2025-06-15 23:55:26 +0800

    fix potential cast exception in world list

--- common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/handler/world/WorldListWidgetCursorHandler.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/handler/world/WorldListWidgetCursorHandler.java
@@ -6,14 +6,19 @@ import io.github.fishstiz.minecraftcursor.api.CursorType;
 import net.minecraft.client.gui.screens.worldselection.WorldSelectionList;
 
 public class WorldListWidgetCursorHandler implements CursorHandler<WorldSelectionList> {
+    private static final int ICON_WIDTH = 32;
+
     @Override
     public CursorType getCursorType(WorldSelectionList worldListWidget, double mouseX, double mouseY) {
         if (!MinecraftCursor.CONFIG.isWorldIconEnabled()) return CursorType.DEFAULT;
 
-        int x = (int) Math.floor((double) worldListWidget.getWidth() / 2 - (double) worldListWidget.getRowWidth() / 2);
-        for (WorldSelectionList.Entry entry : worldListWidget.children()) {
-            if (entry.isMouseOver(mouseX, mouseY) && mouseX >= x && mouseX <= x + 32 && ((WorldSelectionList.WorldListEntry) entry).canJoin()) {
-                return CursorType.POINTER;
+        if (mouseX <= worldListWidget.getRowLeft() + ICON_WIDTH) {
+            for (WorldSelectionList.Entry entry : worldListWidget.children()) {
+                if (entry instanceof WorldSelectionList.WorldListEntry worldListEntry
+                    && worldListEntry.isMouseOver(mouseX, mouseY)
+                    && worldListEntry.canJoin()) {
+                    return CursorType.POINTER;
+                }
             }
         }
         return CursorType.DEFAULT;
