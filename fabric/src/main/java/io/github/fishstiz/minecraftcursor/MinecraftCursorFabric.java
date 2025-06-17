a42df17d3f95007fd21e1f9da724e7825a0923e8
fishstiz <fishstickz7777@gmail.com>
2025-06-17 12:58:53 +0800

    refactors

--- fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
+++ fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
@@ -1,34 +1,47 @@
 package io.github.fishstiz.minecraftcursor;
 
+import io.github.fishstiz.minecraftcursor.cursor.resolver.ElementInspector;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 
 public class MinecraftCursorFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MinecraftCursor.init();
-
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new CursorResourceReloadListener());
         ScreenEvents.BEFORE_INIT.register(this::onScreenInit);
-        ClientTickEvents.START_CLIENT_TICK.register(MinecraftCursor::onClientTick);
+        ClientTickEvents.START_CLIENT_TICK.register(MinecraftCursor::afterClientTick);
+        HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.addLayer(new InspectorLayer()));
     }
 
     private void onScreenInit(Minecraft minecraft, Screen screen, int width, int height) {
-        MinecraftCursor.onScreenInit(minecraft, screen);
+        MinecraftCursor.beforeScreenInit(minecraft, screen);
         ScreenEvents.afterRender(screen).register(this::onScreenRender);
     }
 
     private void onScreenRender(Screen screen, GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        MinecraftCursor.onScreenRender(MinecraftHolder.INSTANCE, screen, guiGraphics, mouseX, mouseY);
+        MinecraftCursor.afterScreenRender(Minecraft.getInstance(), screen, guiGraphics, mouseX, mouseY);
     }
 
-    private static class MinecraftHolder {
-        private static final Minecraft INSTANCE = Minecraft.getInstance();
+    private static class InspectorLayer implements IdentifiedLayer {
+        @Override
+        public ResourceLocation id() {
+            return ElementInspector.getId();
+        }
+
+        @Override
+        public void render(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+            MinecraftCursor.renderInspector(Minecraft.getInstance(), guiGraphics);
+        }
     }
 }
--- neoforge/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorNeoforge.java
+++ neoforge/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorNeoforge.java
@@ -1,16 +1,17 @@
 package io.github.fishstiz.minecraftcursor;
 
+import io.github.fishstiz.minecraftcursor.cursor.resolver.ElementInspector;
 import io.github.fishstiz.minecraftcursor.gui.screen.ConfigurationScreen;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
-import net.neoforged.neoforge.client.event.ClientTickEvent;
-import net.neoforged.neoforge.client.event.ScreenEvent;
+import net.neoforged.neoforge.client.event.*;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 
@@ -24,33 +25,38 @@ public class MinecraftCursorNeoforge {
                 new ConfigurationScreen(screen)
         );
 
+        modEventBus.addListener(InitializeClientRegistriesEvent.class, event ->
+                MinecraftCursor.init()
+        );
         modEventBus.addListener(AddClientReloadListenersEvent.class, event -> {
             var listener = new CursorResourceReloadListener();
             event.addListener(listener.getId(), listener);
         });
+        modEventBus.addListener(RegisterGuiLayersEvent.class, event ->
+                event.registerAboveAll(ElementInspector.getId(), this::onGuiRender)
+        );
 
-        MinecraftCursor.init();
         NeoForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
     public void onScreenInit(ScreenEvent.Init.Pre event) {
         Screen screen = event.getScreen();
-        MinecraftCursor.onScreenInit(screen.getMinecraft(), screen);
+        MinecraftCursor.beforeScreenInit(screen.getMinecraft(), screen);
     }
 
     @SubscribeEvent
     public void onScreenRender(ScreenEvent.Render.Post event) {
         Screen screen = event.getScreen();
-        MinecraftCursor.onScreenRender(screen.getMinecraft(), screen, event.getGuiGraphics(), event.getMouseX(), event.getMouseY());
+        MinecraftCursor.afterScreenRender(screen.getMinecraft(), screen, event.getGuiGraphics(), event.getMouseX(), event.getMouseY());
     }
 
     @SubscribeEvent
     public void onClientTick(ClientTickEvent.Post event) {
-        MinecraftCursor.onClientTick(MinecraftHolder.INSTANCE);
+        MinecraftCursor.afterClientTick(Minecraft.getInstance());
     }
 
-    private static class MinecraftHolder {
-        private static final Minecraft INSTANCE = Minecraft.getInstance();
+    private void onGuiRender(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        MinecraftCursor.renderInspector(Minecraft.getInstance(), guiGraphics);
     }
 }
