e84efb68841885421b94cb7db905f792586608c3
fishstiz <fishstickz7777@gmail.com>
2025-06-17 18:21:26 +0800

    fix cursor not reverting to default in some cases*
    *after closing a HUD-rendered screen and unlocking the mouse without another screen open.

--- fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
+++ fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
@@ -1,12 +1,9 @@
 package io.github.fishstiz.minecraftcursor;
 
-import io.github.fishstiz.minecraftcursor.cursor.resolver.ElementInspector;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
@@ -17,21 +14,15 @@ public class MinecraftCursorFabric implements ClientModInitializer {
     public void onInitializeClient() {
         MinecraftCursor.init();
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new CursorResourceReloadListener());
+        ClientTickEvents.END_CLIENT_TICK.register(MinecraftCursor::afterClientTick);
         ScreenEvents.BEFORE_INIT.register(this::onScreenInit);
-        ClientTickEvents.START_CLIENT_TICK.register(MinecraftCursor::afterClientTick);
-        HudElementRegistry.addFirst(ElementInspector.getId(), this::onHudRender);
     }
 
     private void onScreenInit(Minecraft minecraft, Screen screen, int width, int height) {
-        MinecraftCursor.beforeScreenInit(minecraft, screen);
         ScreenEvents.afterRender(screen).register(this::onScreenRender);
     }
 
     private void onScreenRender(Screen screen, GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        MinecraftCursor.afterScreenRender(Minecraft.getInstance(), screen, guiGraphics, mouseX, mouseY);
-    }
-
-    private void onHudRender(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
-        MinecraftCursor.renderInspector(Minecraft.getInstance(), guiGraphics);
+        MinecraftCursor.afterCurrentScreenRender(Minecraft.getInstance(), screen, guiGraphics, mouseX, mouseY);
     }
 }
