cf9fab0a34ff5d59573a1ea0c59787c5c1d35453
fishstiz <fishstickz7777@gmail.com>
2025-06-17 12:58:53 +0800

    refactors

--- common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/resolver/ElementInspector.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/cursor/resolver/ElementInspector.java
@@ -1,17 +1,22 @@
 package io.github.fishstiz.minecraftcursor.cursor.resolver;
 
+import io.github.fishstiz.minecraftcursor.MinecraftCursor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 public interface ElementInspector {
     ElementInspector NO_OP = new ElementInspector() {
     };
 
+    static ResourceLocation getId() {
+        return MinecraftCursor.loc("inspector");
+    }
+
     default void destroy() {
-        // no-op
     }
 
     default boolean setProcessed(GuiEventListener processed, boolean cached) {
@@ -19,15 +24,9 @@ public interface ElementInspector {
     }
 
     default void render(Minecraft minecraft, @NotNull Screen screen, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-        // no-op
     }
 
     default boolean isInspecting() {
-        return this != NO_OP;
-    }
-
-    static ElementInspector toggle(ElementInspector elementInspector) {
-        elementInspector.destroy();
-        return elementInspector instanceof ElementInspectorImpl ? NO_OP : new ElementInspectorImpl();
+        return false;
     }
 }
