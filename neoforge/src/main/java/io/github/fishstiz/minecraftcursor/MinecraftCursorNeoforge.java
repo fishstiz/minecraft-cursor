2bfe01244a1b2809224a28dbaf532f810decce29
fishstiz <fishstickz7777@gmail.com>
2025-06-17 18:21:26 +0800

    fix cursor not reverting to default in some cases*
    *after closing a HUD-rendered screen and unlocking the mouse without another screen open.

--- neoforge/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorNeoforge.java
+++ neoforge/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorNeoforge.java
@@ -1,10 +1,8 @@
 package io.github.fishstiz.minecraftcursor;
 
-import io.github.fishstiz.minecraftcursor.cursor.resolver.ElementInspector;
+import io.github.fishstiz.minecraftcursor.config.Flag;
 import io.github.fishstiz.minecraftcursor.gui.screen.ConfigurationScreen;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
@@ -21,42 +19,29 @@ public class MinecraftCursorNeoforge {
     public static final String MOD_ID = "minecraft_cursor";
 
     public MinecraftCursorNeoforge(ModContainer container, IEventBus modEventBus) {
+        Flag.REMAP.disable();
+
         container.registerExtensionPoint(IConfigScreenFactory.class, (c, screen) ->
                 new ConfigurationScreen(screen)
         );
 
-        modEventBus.addListener(InitializeClientRegistriesEvent.class, event ->
-                MinecraftCursor.init()
-        );
         modEventBus.addListener(AddClientReloadListenersEvent.class, event -> {
+            MinecraftCursor.init();
             var listener = new CursorResourceReloadListener();
             event.addListener(listener.getId(), listener);
         });
-        modEventBus.addListener(RegisterGuiLayersEvent.class, event ->
-                event.registerAboveAll(ElementInspector.getId(), this::onGuiRender)
-        );
 
         NeoForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void onScreenInit(ScreenEvent.Init.Pre event) {
-        Screen screen = event.getScreen();
-        MinecraftCursor.beforeScreenInit(screen.getMinecraft(), screen);
-    }
-
-    @SubscribeEvent
-    public void onScreenRender(ScreenEvent.Render.Post event) {
-        Screen screen = event.getScreen();
-        MinecraftCursor.afterScreenRender(screen.getMinecraft(), screen, event.getGuiGraphics(), event.getMouseX(), event.getMouseY());
-    }
-
-    @SubscribeEvent
-    public void onClientTick(ClientTickEvent.Post event) {
+    private void afterClientTick(ClientTickEvent.Post event) {
         MinecraftCursor.afterClientTick(Minecraft.getInstance());
     }
 
-    private void onGuiRender(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
-        MinecraftCursor.renderInspector(Minecraft.getInstance(), guiGraphics);
+    @SubscribeEvent
+    private void afterScreenRender(ScreenEvent.Render.Post event) {
+        Screen screen = event.getScreen();
+        MinecraftCursor.afterCurrentScreenRender(screen.getMinecraft(), screen, event.getGuiGraphics(), event.getMouseX(), event.getMouseY());
     }
 }
