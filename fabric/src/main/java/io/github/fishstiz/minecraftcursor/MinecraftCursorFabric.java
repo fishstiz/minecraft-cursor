f688c7d179b1633cf983431c1bafe4c02ff26833
fishstiz <fishstickz7777@gmail.com>
2025-06-17 12:58:53 +0800

    refactors

--- fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
+++ fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
@@ -1,9 +1,12 @@
 package io.github.fishstiz.minecraftcursor;
 
+import io.github.fishstiz.minecraftcursor.cursor.resolver.ElementInspector;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
@@ -13,22 +16,22 @@ public class MinecraftCursorFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         MinecraftCursor.init();
-
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new CursorResourceReloadListener());
         ScreenEvents.BEFORE_INIT.register(this::onScreenInit);
-        ClientTickEvents.START_CLIENT_TICK.register(MinecraftCursor::onClientTick);
+        ClientTickEvents.START_CLIENT_TICK.register(MinecraftCursor::afterClientTick);
+        HudElementRegistry.addFirst(ElementInspector.getId(), this::onHudRender);
     }
 
     private void onScreenInit(Minecraft minecraft, Screen screen, int width, int height) {
-        MinecraftCursor.onScreenInit(minecraft, screen);
+        MinecraftCursor.beforeScreenInit(minecraft, screen);
         ScreenEvents.afterRender(screen).register(this::onScreenRender);
     }
 
     private void onScreenRender(Screen screen, GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        MinecraftCursor.onScreenRender(MinecraftHolder.INSTANCE, screen, guiGraphics, mouseX, mouseY);
+        MinecraftCursor.afterScreenRender(Minecraft.getInstance(), screen, guiGraphics, mouseX, mouseY);
     }
 
-    private static class MinecraftHolder {
-        private static final Minecraft INSTANCE = Minecraft.getInstance();
+    private void onHudRender(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        MinecraftCursor.renderInspector(Minecraft.getInstance(), guiGraphics);
     }
 }
