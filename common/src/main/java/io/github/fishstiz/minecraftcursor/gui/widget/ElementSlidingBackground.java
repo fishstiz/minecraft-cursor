d8d1f6b3b2e98d15dd31e7f35bf1524a4879baf8
fishstiz <fishstickz7777@gmail.com>
2025-06-20 02:45:33 +0800

    more consistent sliding background speed

--- common/src/main/java/io/github/fishstiz/minecraftcursor/gui/widget/ElementSlidingBackground.java
+++ common/src/main/java/io/github/fishstiz/minecraftcursor/gui/widget/ElementSlidingBackground.java
@@ -1,14 +1,17 @@
 package io.github.fishstiz.minecraftcursor.gui.widget;
 
+import net.minecraft.Util;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ElementSlidingBackground {
+    private static final float ANIMATION_SPEED = 25;
     private final boolean outlineOnly;
     private final int color;
     private float lastY = 0;
+    private long lastFrameTime = Util.getNanos();
 
     public ElementSlidingBackground(int color, boolean outlineOnly) {
         this.color = color;
@@ -21,15 +24,21 @@ public class ElementSlidingBackground {
 
     public void reset() {
         this.lastY = 0;
+        this.lastFrameTime = Util.getNanos();
     }
 
     public void render(@NotNull GuiGraphics guiGraphics, int x, int y, int width, int height, float partialTick) {
+        long now = Util.getNanos();
+        float deltaTime = (now - this.lastFrameTime) / 1_000_000_000f;
+        this.lastFrameTime = now;
+
         this.lastY = this.lastY == 0 ? y : this.lastY;
 
         if (Math.abs(this.lastY - y) < 0.5f) {
             this.lastY = y;
         } else {
-            this.lastY = Mth.lerp(partialTick, this.lastY, y);
+            float alpha = 1f - (float) Math.exp(-ANIMATION_SPEED * deltaTime);
+            this.lastY = Mth.lerp(alpha, this.lastY, y);
         }
 
         int right = x + width;
