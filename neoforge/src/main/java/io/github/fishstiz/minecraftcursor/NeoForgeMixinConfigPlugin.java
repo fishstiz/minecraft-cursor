7a64c10a28c096b9f446e1b63a606ef9c3886fef
fishstiz <fishstickz7777@gmail.com>
2025-06-23 05:52:25 +0800

    neoforge owo-lib compat (#38)

---
+++ neoforge/src/main/java/io/github/fishstiz/minecraftcursor/NeoForgeMixinConfigPlugin.java
@@ -0,0 +1,67 @@
+package io.github.fishstiz.minecraftcursor;
+
+import io.github.fishstiz.minecraftcursor.config.Flag;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.fml.loading.LoadingModList;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class NeoForgeMixinConfigPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+        // do nothing
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (!FMLEnvironment.dist.isClient()) {
+            return false;
+        }
+
+        final String mixinPackage = "io.github.fishstiz.minecraftcursor.mixin";
+
+        if (mixinClassName.startsWith(mixinPackage + ".compat.ftblibrary") && this.isModLoaded("ftblibrary")) {
+            return true;
+        }
+        if (mixinClassName.startsWith(mixinPackage + ".compat.owo") && this.isModLoaded("owo")) {
+            return true;
+        }
+
+        Flag.REMAP.disable();
+
+        return false;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+        // do nothing
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // do nothing
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // do nothing
+    }
+
+    private boolean isModLoaded(String modId) {
+        return LoadingModList.get().getModFileById(modId) != null;
+    }
+}
