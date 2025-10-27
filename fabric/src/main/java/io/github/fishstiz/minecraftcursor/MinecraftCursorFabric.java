47b7096ba79567e828a2c93203c5a6aacce724e8
fishstiz <fishstickz7777@gmail.com>
2025-10-28 04:55:14 +0800

    backport cursors extended textures as resource pack (#42)

--- fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
+++ fabric/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorFabric.java
@@ -4,9 +4,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.PackType;
 
 public class MinecraftCursorFabric implements ClientModInitializer {
@@ -14,6 +17,14 @@ public class MinecraftCursorFabric implements ClientModInitializer {
     public void onInitializeClient() {
         MinecraftCursor.init();
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new CursorResourceReloadListener());
+        FabricLoader.getInstance().getModContainer(MinecraftCursor.MOD_ID).ifPresent(modContainer ->
+                ResourceManagerHelper.registerBuiltinResourcePack(
+                        MinecraftCursor.loc("cursors_extended"),
+                        modContainer,
+                        Component.literal("Cursors Extended"),
+                        ResourcePackActivationType.NORMAL
+                )
+        );
         ClientTickEvents.END_CLIENT_TICK.register(MinecraftCursor::afterClientTick);
         ScreenEvents.BEFORE_INIT.register(this::onScreenInit);
     }
--- forge/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorForge.java
+++ forge/src/main/java/io/github/fishstiz/minecraftcursor/MinecraftCursorForge.java
@@ -2,14 +2,26 @@ package io.github.fishstiz.minecraftcursor;
 
 import io.github.fishstiz.minecraftcursor.gui.screen.ConfigurationScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.forgespi.language.IModInfo;
+
+import java.nio.file.Path;
 
 @Mod(MinecraftCursorForge.MOD_ID)
 public class MinecraftCursorForge {
@@ -30,6 +42,27 @@ public class MinecraftCursorForge {
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
+        @SubscribeEvent
+        public static void addPackFinder(AddPackFindersEvent event) {
+            ResourceLocation location = ResourceLocation.fromNamespaceAndPath(MOD_ID, "resourcepacks/cursors_extended");
+            String packId = "mod/" + location;
+
+            IModInfo modInfo = ModList.get().getModContainerById(MOD_ID).map(ModContainer::getModInfo).orElseThrow();
+            Path path = modInfo.getOwningFile().getFile().findResource(location.getPath());
+
+            Pack pack = Pack.readMetaAndCreate(
+                    packId,
+                    Component.literal("Cursors Extended"),
+                    false,
+                    id -> new PathPackResources(id, path, false),
+                    PackType.CLIENT_RESOURCES,
+                    Pack.Position.TOP,
+                    PackSource.BUILT_IN
+            );
+
+            event.addRepositorySource(consumer -> consumer.accept(pack));
+        }
+
         @SubscribeEvent
         public static void registerReloadListeners(RegisterClientReloadListenersEvent event) {
             MinecraftCursor.init();
